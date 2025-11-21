2249c0bb62c7398679f67957bdf525fbf711242a
Anurag <anuragbhelsewale21@gmail.com>
2025-11-21 13:47:27 +0530

    deploy final

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/react-core-app/",
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
 });
